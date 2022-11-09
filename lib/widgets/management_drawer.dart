2627b09d8a0ff807684f8e337aee7ebbe658d75f
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-11-09 23:18:00 +0600

    refactoring the drawer lists

---
+++ lib/widgets/management_drawer.dart
@@ -0,0 +1,55 @@
+import 'package:club_election_app_flutter/widgets/drawer_list_tile.dart';
+import 'package:flutter/material.dart';
+
+class ManagementDrawer extends StatelessWidget {
+  const ManagementDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          DrawerListTile(
+            label: 'Clubs',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          DrawerListTile(
+            label: 'Candidates',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          DrawerListTile(
+            label: 'Voters',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          DrawerListTile(
+            label: 'Settings',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          DrawerListTile(
+            label: 'Logout',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
