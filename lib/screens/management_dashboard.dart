e6adf4f7947d71d9129e337a9f469a2ae055818c
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-11-09 23:18:15 +0600

    adding drawer in management screen

--- lib/screens/management_dashboard.dart
+++ lib/screens/management_dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:club_election_app_flutter/widgets/management_drawer.dart';
 import 'package:flutter/material.dart';
 
 class ManagementDashboard extends StatelessWidget {
@@ -10,6 +11,7 @@ class ManagementDashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Management Dashboard'),
       ),
+      drawer: const ManagementDrawer(),
       body: const Center(
         child: Text('Management Dashboard'),
       ),
